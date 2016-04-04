bc61b00c03302d7542b74a5f16af42a90d5121f0
Shandra Iannucci <shandrai@gmail.com>
2016-04-04 10:45:05 -0700

    Add jQuery functionality

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
+    $(window).keydown(function(e) {
+    	if (e.which === 88) {
+    		$('.ryu').children().hide();
+    		$('.ryu-cool').show();
+    	}
+    })
+    .keyup(function(e) {
+    	if (e.which === 88) {
+    		$('.ryu-cool').hide();
+    		$('.ryu-still').show();
+    	}
+    });
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
